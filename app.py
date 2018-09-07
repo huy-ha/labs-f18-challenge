a3e096c5e9bf97fb3f4f17bd5999ec906f3d784c
huy-ha <haquochuy@gmail.com>
2018-09-07 13:39:35 -0400

    GET requests to pokeapi for pokemon data
    implemented fetches to pokeapi using request
    extract relevant information from json response
    passed string over to index.html

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
+import requests
+import json
+import re
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def main():
-    return render_template('index.html')
+@app.route('/pokemon/<variable>', methods=['GET'])
+def main(variable):
+	url = 'http://pokeapi.co/api/v2/pokemon/' + variable
+	r = requests.get(url)
+	url = r.json()['forms'][0]['url']
+	name = r.json()['forms'][0]['name']
+	id = re.search('(?<=pokemon-form\/)\d+',url).group(0)
+
+	
+	if variable.isdigit():
+		value = "The pokemon with id " + id + " is " + name
+	else:
+		value = name + " has id " + id
+	return render_template('index.html',value = value)
 
 
 if __name__ == '__main__':
